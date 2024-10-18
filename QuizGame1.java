4f9d78d738cd3fd663dfa2045e24b3fcf3dc8d9c
Maki-303 <markjagonap65@gmail.com>
2024-10-19 07:42:10 +0800

    Add files via upload

---
+++ QuizGame1.java
@@ -0,0 +1,72 @@
+package markjagonap;
+import java.util.Scanner;
+public class QuizGame1 {
+public static void main(String[] args) {
+	
+	try {
+		  Scanner s = new Scanner(System.in);
+	
+   System.out.println("---=Welcome to Quiz game!=----");
+   System.out.println("---=Enter Yes to continue=--");
+   System.out.println("----=Enter any to exit=--");
+	      
+	 String continueGame = s.nextLine();
+		if (!continueGame.equalsIgnoreCase("yes")) 
+		{ System.out.println("Exiting the game. Goodbye!");
+	     return; 
+	    }
+
+	  String question1 = "Where is the Bermuda Triangle located?\n[a] The Atlantic Ocean\n[b] The Pacific Ocean\n[c] Arctic Ocean";
+	  String answer1 = "a"; 
+	  String question2 = "Which planet is closest to the earth?\n[a] Jupiter\n[b] Saturn\n[c] Mercury";
+	  String answer2 = "c";
+	  String question3 = "What is the 5th decimal digit of pi?\n[a] 8\n[b] 9\n[c] 7";
+	  String answer3 = "b";
+	  int score = 0;
+
+	  System.out.println("Question number 1");
+	  System.out.println(question1);
+	  String userAnswer1 = s.nextLine().toLowerCase(); 
+
+	  if (userAnswer1.equals(answer1)) {
+	  System.out.println("Your Correct!");
+	  score++; 
+	  } 
+	  else {
+	  System.out.println("Your Wrong Next Question");
+	  }
+	  System.out.println("Question number 2");
+	  System.out.println(question2);
+	  String userAnswer2 = s.nextLine().toLowerCase(); 
+
+	  if (userAnswer2.equals(answer2)) {
+	    System.out.println("Your Correct!");
+	    score++;
+	   } else {
+	   System.out.println("Your Wrong Next Question");
+	   }
+
+	   System.out.println("Question number 3");
+	   System.out.println(question3);
+	   String userAnswer3 = s.nextLine().toLowerCase(); 
+
+	  if (userAnswer3.equals(answer3)) {
+	  System.out.println("Your Correct!");
+	  score++; 
+	  } else {
+	    System.out.println("Your Wrong Next Question");
+	  }
+
+	System.out.println("---=The Result of the Quiz game!=----");
+	System.out.println("#1 Your " + (userAnswer1.equals(answer1) ? "Correct" : "Wrong") + " Correct Answer [" + answer1 + "] " + question1);
+	System.out.println("#2 Your " + (userAnswer2.equals(answer2) ? "Correct" : "Wrong") + " Correct Answer [" + answer2 + "] " + question2);
+	System.out.println("#3 Your " + (userAnswer3.equals(answer3) ? "Correct" : "Wrong") + " Correct Answer [" + answer3 + "] " + question3);
+	        
+	System.out.println("Your Total Score is " + score + "/3 Better Luck Next Time!");
+	    s.close();
+	        
+	    }catch (Exception e ) {
+	    	System.out.println ("Enter Letter only");
+	    }
+	}}
+
---
+++ dateFormatter.java
@@ -0,0 +1,98 @@
+package markjagonap;
+import java.util.Scanner;
+public class dateFormatter {
+public static void main(String[] args) {
+		
+	Scanner s = new Scanner(System.in);
+	
+	System.out.println("Month  : ");
+	int month = s.nextInt();
+	System.out.println("Day    : ");
+	int day = s.nextInt();
+	System.out.println("Year   : ");
+	int year = s.nextInt();
+	
+	
+	  if (month < 1 || month >12 ) {
+		System.out.println("Invalid Month or Day");
+		return;
+	}
+	  if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+		   System.out.println("Invalid month or day");
+		   return;
+	}
+	  if (month == 2) {
+		  if (day < 1 || day > 29) {
+		  System.out.println("Invalid month or day");
+		  return;
+		  }
+	  if (day == 29 && ! isLeapYear (year)) {
+		  System.out.println ("Invalid Month or day");
+	  }
+	  
+	  }
+	  if (day < 1  || day > 31 ) {
+		  System.out.println ("Invalid Month Or day");
+		 return;
+	  }
+	  
+	  String monthname = "";
+	  switch (month) {
+	                 case 1 : monthname = "January"; break;
+	                 case 2 : monthname = "January"; break;
+	                 case 3 : monthname = "January"; break;
+	                 case 4 : monthname = "January"; break;
+	                 case 5 : monthname = "January"; break;
+	                 case 6 : monthname = "January"; break;
+	                 case 7 : monthname = "January"; break;
+	                 case 8 : monthname = "January"; break;
+	                 case 9 : monthname = "January"; break;
+	                 case 10: monthname = "January"; break;
+	                 case 11: monthname = "January"; break;
+	                 case 12: monthname = "January"; break;
+	  }   
+	  
+	   String occasion = "";  
+	   switch (month) { 
+	   					case 1 :
+	   						if (day == 1) occasion = "New Year's Day"; 
+	   						                  break;
+	   					case 2 :
+	   						if (day == 1) occasion = "Chinese New Year"; 
+	   						else if (day == 14 )  occasion = "Valentines Day"; 
+	   						                  break;
+	   					case 3 :
+	   						if (day == 28) occasion = "Maundy Thursday"; 
+	   						else if (day == 29) occasion = " Good Friday";
+	   						else if (day == 30) occasion = "black Staurday"; 
+	   						else if (day == 9) occasion = " Day of Valor"; 
+	   						                  break;
+	   					case 4 :
+	   						if (day == 10) occasion ="Eidul fitr"; 
+	   						                  break;
+	   					case 5 :
+	   						if (day == 1) occasion = ""; 
+	   						                  break;
+	   						
+	   					case 6 :
+	   						if (day == 1) occasion = ""; break;
+	   					case 8 :
+	   						if (day == 1) occasion = ""; break;
+	   					case 11:
+	   						if (day == 1) occasion = ""; break;
+	   					case 12:
+	   						if (day == 1) occasion = ""; break;
+	   					
+	   		
+	             
+	  }
+			  
+
+	} //catch  
+
+}
+
+// public static void boolean isLeapYear (int year) {
+//	 return (year & 4 == 0 && year % 100!=0) || (year % == -0);
+// }
+
